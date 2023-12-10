d9d153b6ddb11acf0b978397bad343887c7e3663
Raul del Aguila <38803643+raulaguila@users.noreply.github.com>
2023-12-09 20:29:27 -0400

    updated swagger docs

--- pkg/helpers/duration.go
+++ pkg/helpers/duration.go
@@ -6,10 +6,12 @@ import (
 	"time"
 )
 
+var ErrIntConvert error = errors.New("invalid string number")
+
 func DurationFromString(str string, factor time.Duration) (time.Duration, error) {
 	converted, err := strconv.Atoi(str)
 	if err != nil {
-		return time.Duration(0), errors.New("invalid string")
+		return time.Duration(0), ErrIntConvert
 	}
 
 	return time.Duration(converted) * factor, nil
